7a232cc97d948ee6c84457d3c15bfec124ac4553
yanikr <94018380+yanikr@users.noreply.github.com>
2022-08-27 21:14:48 +0000

    Deploying to gh-pages from @ yanikr/goit-js-hw-09@79c3058d54d73d9c48a0f57caa5b5a00261ad462 🚀

---
+++ 01-color-switcher.2a89f5f0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.2a89f5f0.js.map
---
+++ 01-color-switcher.4a39825d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.4a39825d.js.map
